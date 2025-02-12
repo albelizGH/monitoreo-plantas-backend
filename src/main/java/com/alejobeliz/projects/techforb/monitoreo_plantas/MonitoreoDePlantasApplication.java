f10d7c230ee29794d93da9d876aa405d02e9b173
Alejo Beliz <129092769+albelizGH@users.noreply.github.com>
2025-02-12 02:39:51 -0300

    Primer commit

---
+++ src/main/java/com/alejobeliz/projects/techforb/monitoreo_plantas/MonitoreoDePlantasApplication.java
@@ -0,0 +1,13 @@
+package com.alejobeliz.projects.techforb.monitoreo_plantas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoreoDePlantasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoreoDePlantasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alejobeliz/projects/techforb/monitoreo_plantas/MonitoreoDePlantasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejobeliz.projects.techforb.monitoreo_plantas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoreoDePlantasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
